45d6ac8679e29b165da2355a3f756585aebcfdd4
kubaczechowski <jcobczechowski@gmail.com>
2021-11-27 23:11:28 +0100

    scratch for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./app/home-page/home-page.component";
+
 
 const routes: Routes = [
   {
     path: "collections",
     loadChildren: () => import('./collections/collections.module')
-      .then(f=>f.CollectionsModule)
+      .then(f=>f.CollectionsModule),
+  },
+  {
+    path: "home",
+    component: HomePageComponent
   }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NavbarComponent } from './app/shared/navbar/navbar.component';
+import { HomePageComponent } from './app/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
