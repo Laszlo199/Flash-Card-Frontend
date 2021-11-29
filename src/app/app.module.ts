7700ee6f696d2697207a0c9805f570f5427f6956
kamila0801 <kami7686@easv365.dk>
2021-11-29 22:53:54 +0100

    the end of this mess.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { NavbarComponent } from './home-page/navbar/navbar.component';
-import { HomePageComponent } from './home-page/home-page/home-page.component';
-import { FooterComponent } from './home-page/footer/footer.component';
-import { PopularDecksComponent } from './home-page/popular-decks/popular-decks.component';
 import {CollectionsModule} from "./collections/collections.module";
 import {HomePageModule} from "./home-page/home-page.module";
 
