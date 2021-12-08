2b0c43c1adad8bc0fb157e094a3070237ec8bdcc
kubaczechowski <jcobczechowski@gmail.com>
2021-12-08 16:16:51 +0100

    added routing

--- src/app/collections/navbar-collections/navbar-collections.component.ts
+++ src/app/collections/navbar-collections/navbar-collections.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../auth/shared/auth.service";
 
 @Component({
   selector: 'app-navbar-collections',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarCollectionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router ,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  goToCollections() {
+    this.router.navigateByUrl("/collections");
+  }
+
+  logOut() {
+    this.router.navigateByUrl("/home");
+    this.authService.logout();
+  }
 }
