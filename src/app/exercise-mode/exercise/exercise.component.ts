1f43d85b561e6a0ce5e9d4c3a367f7b0d66cb298
soma199 <70133057+Laszlo199@users.noreply.github.com>
2021-12-14 13:54:30 +0100

    exercise mode

--- src/app/exercise-mode/exercise/exercise.component.ts
+++ src/app/exercise-mode/exercise/exercise.component.ts
@@ -15,12 +15,10 @@ export class ExerciseComponent implements OnInit {
   deckId : number | undefined;
   deck: exerciseDecksDto | undefined;
   cards: exerciseCardsDto[] | undefined;
-  wrongCards: exerciseCardsDto[] | undefined;
   index: number = 0;
   toggleProperty = false;
   answer: string | undefined;
   checked: boolean = false;
-  answeredCorrectly: boolean | undefined;
 
 
   constructor(private _service: ExerciseService,
@@ -56,19 +54,50 @@ export class ExerciseComponent implements OnInit {
     this.toggleProperty = !this.toggleProperty;
   }
 
-  checkAnswer() {
-    if(!this.checked && this.answer && this.cards) {
-      this.checked = true;
+  async checkAnswer() {
 
-      if (this.answer.toLowerCase() == this.cards[this.index].answer.toLowerCase()) {
-        this.answeredCorrectly = true;
-        
+      if (!this.checked && this.answer && this.cards) {
+        this.checked = true;
+
+        if (this.answer.toLowerCase() == this.cards[this.index].answer.toLowerCase()) {
+
+          await new Promise(f => setTimeout(f, 4000));
+          this.toggle();
+          this.hideAnswer();
+          if (this.index+1 == this.cards.length) {
+            this.index = 0;
+            this.cards.splice(this.cards.length-1, 1);
+          }else {
+            this.cards.splice(this.index, 1);
+          }
+          console.log('good');
+        } else {
+          console.log('wrong');
+          this.skip();
+        }
       }
-    }
-
   }
 
-  skip() {
-    this.index++;
+  hideAnswer() {
+    this.checked = false;
+  }
+
+  async skip() {
+    if (this.index + 1 == this.cards?.length || this.index == this.cards?.length) {
+      this.next();
+      this.index = 0;
+    } else {
+     this.next();
+      this.index++;
+    }
+  }
+
+  async next() {
+    this.checked = true;
+    await new Promise(f => setTimeout(f, 4000));
+    this.hideAnswer();
+    this.checked = false;
+    this.toggle();
+
   }
 }
